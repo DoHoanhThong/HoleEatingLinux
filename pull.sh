04663ad814aac9e295c9f294077cff79493ec543
Server wecraft 2 <server>
2025-06-27 10:16:43 +0000

    updte

--- pull.sh
+++ pull.sh
@@ -1,1 +1,1 @@
-git reset --hard && git pull && chmod +x ./HoleEatingSV.x86_64  && pm2 start ecosystem.config.js
+git reset --hard && git pull && chmod +x ./HoleEatingSV.x86_64  && /home/ubuntu/.nvm/versions/node/v22.10.0/bin/pm2 start ecosystem.config.js
